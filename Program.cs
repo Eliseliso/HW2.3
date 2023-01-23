1cf4bbc139794631d1569fde3e2694156c86792f
Elis <eliso1190@yandex.ru>
2023-01-24 02:20:47 +0300

    First

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите цифру дня недели: ");
+int num = int.Parse(Console.ReadLine());
+if (num <= 5)
+{
+    Console.WriteLine("Нет");
+}
+if (num == 6)
+{
+    Console.WriteLine("Да");
+}
+else
+if (num == 7)
+{
+    Console.WriteLine("Да");
+}
